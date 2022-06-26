a3b852cd7fe2248b45eb0d9e3c0c83baf4789d0d
JustAnotherN3rd <tcora.volb@gmail.com>
2022-06-26 17:58:04 +0200

    db

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
-#db = SQL("sqlite:///shield.db")
+db = SQL("sqlite:///kanban.db")
 
 # set current mission to NULL
 current_mission = {}
