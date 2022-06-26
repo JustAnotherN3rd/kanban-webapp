48f9bb8a23c2fe78598ed7823916ccb9cbacc682
JustAnotherN3rd <tcora.volb@gmail.com>
2022-06-26 17:02:12 +0200

    some changes

--- app.py
+++ app.py
@@ -1,9 +1,76 @@
-import requests
+from importlib import import_module
+import os
 
-from flask import Flask, render_template
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from helpers import login_required
+
 
 
 app = Flask(__name__)
+
+
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use SQLite database
+#db = SQL("sqlite:///shield.db")
+
+# set current mission to NULL
+current_mission = {}
+
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("Provide Username!")
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("Provide Password!")
+
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return apology("Invlaid Password!")
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
 
 
 @app.route("/")
